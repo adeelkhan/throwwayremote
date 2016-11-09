4235ffeb7fed08e4f170e13b4c838258a6b4d737
Muhammad Adeel Khan <adeelkhan@Muhammads-MacBook-Pro.local>
2016-11-09 18:06:51 +0500

    adds difficulty filter for questions

--- myapp/views.py
+++ myapp/views.py
@@ -25,12 +25,27 @@ def dashboard(request):
 def questions_list(request):
     dict = {}
     sub_topic_id = request.POST.get("sub_topic")
+
+    difficulty_level = request.POST.get("difficulty_level")
     print(sub_topic_id)
-    question_list = Question.objects.filter(sub_topic_id=sub_topic_id).order_by('-num_of_times_asked')
-    dict["question_list"] = question_list
+    print difficulty_level
+    print(type(difficulty_level))
+
+    if difficulty_level is None or \
+            int(difficulty_level) is 4:
+        print("here")
+        question_list = Question.objects.filter(sub_topic_id=sub_topic_id).order_by('-num_of_times_asked')
+
+    else:
+        print("there")
+        question_list = Question.objects.filter(sub_topic_id=sub_topic_id,
+                                                difficulty_level=difficulty_level).order_by('-num_of_times_asked')
 
 
     print(questions_list)
+
+    dict["question_list"] = question_list
+    dict["sub_topic"] = sub_topic_id
 
     return render(request, 'myapp/questions.html', dict)
 
